03f9515180a4712c3d2b6a4fb57cfbedcf2c0ab2
Salmon Roe <roe.janvier@gmail.com>
2025-07-15 15:49:00 +0800

    fluent validation

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Restaurants;
 
 namespace Restaurants.Application.Extensions;
@@ -7,7 +9,11 @@ public static class ServiceCollectionExtensions
 {
     public static void AddApplication(this IServiceCollection services)
     {
+        var applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
+
+        // Register application services    
         services.AddScoped<IRestaurantsService, RestaurantsService>();
-        services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
+        services.AddAutoMapper(applicationAssembly);
+        services.AddValidatorsFromAssembly(applicationAssembly).AddFluentValidationAutoValidation();
     }
 }
---
+++ Restaurants.Application/Restaurants/Validators/CreateRestaurantValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using Restaurants.Application.Restaurants.DTO;
+using System.Runtime.CompilerServices;
+
+namespace Restaurants.Application.Restaurants.Validators;
+
+public class CreateRestaurantValidator : AbstractValidator<CreateRestaurantDto>
+{
+    private readonly List<string> categories = ["Italian", "Chinese", "Indian", "Mexican", "American", "French", "Japanese", "Mediterranean"];
+
+    public CreateRestaurantValidator()
+    {
+
+        RuleFor(dto => dto.Name)
+            .NotEmpty()
+            .Length(3, 100);
+
+        RuleFor(dto => dto.Description)
+            .NotEmpty().WithMessage("Description is Required.");
+
+        RuleFor(dto => dto.Category)
+            .Must(categories.Contains)
+            .WithMessage("Invalid Category. Please choose from the valid categories.");
+            //.Custom((value, context) =>
+            //{
+            //    bool isValidCategory = categories.Contains(item: value);
+
+            //    if (!isValidCategory)
+            //    {
+            //        context.AddFailure("Category", "Invalid Category. Please choose from the valid categories");
+            //    }
+            //});
+
+
+        RuleFor(dto => dto.ContactEmail)
+            .EmailAddress().WithMessage("Please provide a valid email address.");
+
+        RuleFor(dto => dto.PostalCode)
+            .Matches(@"^\d{2}-\d{3}$").WithMessage("Please provide a valid postal code (XX-XXX).");
+
+
+    }
+}
